7d98c099a80f497ce53e66cfdf491a26c738ce77
Budi <budimanfajarf@gmail.com>
2023-05-13 15:14:36 +0700

    variable & decration by go dev

---
+++ variables-and-declaration/go-dev-variables/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// var package level declaration
+var c, python, java bool
+
+func main() {
+	// var function level declaration
+	var i int
+
+	fmt.Println(i, c, python, java)
+}
